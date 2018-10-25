c693c78539c87fc7847279f1b8b628bd9cd4aa25
Taka Wang <wang.chia.ming@gmail.com>
2018-10-25 10:11:52 +0800

    Fix test case 4.12 with help intent

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import logging
 from flask import Flask
 from flask_ask import Ask, question, session, statement
 
 app = Flask(__name__)
 ask = Ask(app, "/")
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 @app.route('/')
 def homepage():
@@ -10,18 +12,28 @@ def homepage():
 
 @ask.launch
 def start_skill():
-    msg = 'Hello there, would you like to know the inwin?'
-    return question(msg)
+    msg = 'Hello there, would you like to know the InWin?'
+    msg2 = "I didn't get that. Whould you like to know the brand philosophy of InWin"
+    return question(msg).reprompt(msg2).simple_card('Ask Inwin', msg)
 
 @ask.intent("YesIntent")
 def yes_intent():
     msg = 'Contemporary & Innovative defines the InWin brand. It reflects in the appearance and features of our products. With an artistic approach in mind, InWin products integrate technology, functionality, user-friendly, safety, practicality and overall quality, providing an entirely new experience with each product.'
-    return statement(msg)
+    return statement(msg).simple_card('Ask Inwin', msg)
 
 @ask.intent("NoIntent")
 def no_intent():
     msg = 'I am not sure why you asked me to run then, but okay... bye'
-    return statement(msg)
+    return statement(msg).simple_card('Ask Inwin', msg)
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    speech_text = 'You can ask me to tell you the brand philosophy of InWin!'
+    return question(speech_text).reprompt(speech_text).simple_card('Ask Inwin', speech_text)
+
+@ask.session_ended
+def session_ended():
+    return "{}", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
